fdf365ea5ce987289181b360d4c61886552190a9
Dante Alferez Condori <dalcon@hotmail.com>
2023-06-05 00:54:18 -0500

    Using state provider Riverpod.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:app_catalogo/config/models/category.dart';
 import 'package:app_catalogo/presentation/screens/list_products.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'config/theme/app_theme.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+  ProviderScope(child: MyApp())
+);
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'App Catalogo',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 4).theme(),
-      home: ListProductsScreen(category: categories[0],),
+      home: ListProductsScreen(category: categories[0]),
     );
   }
 }
--- lib/presentation/widgets/main_menu.dart
+++ lib/presentation/widgets/main_menu.dart
@@ -1,6 +1,8 @@
 import 'package:app_catalogo/config/models/category.dart';
+import 'package:app_catalogo/presentation/providers/menu_index_provider.dart';
 import 'package:app_catalogo/presentation/screens/list_products.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MainMenu extends StatefulWidget {
 
@@ -18,25 +20,30 @@ class _MainMenuState extends State<MainMenu> {
 
   @override
   Widget build(BuildContext context) {
-    return NavigationDrawer(
-      selectedIndex: navIndex,
-      onDestinationSelected: (value) {
-        setState(() {
-          navIndex = value;
-        });
-
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => ListProductsScreen(category: categories[value]),
-          )
+    return Consumer(
+      builder: (context, ref, child) {
+        final menuIndex = ref.watch(menuIndexProvider);
+        return NavigationDrawer(
+          selectedIndex: menuIndex,
+          onDestinationSelected: (value) {
+            setState(() {
+              //navIndex = value;
+              ref.read(menuIndexProvider.notifier).state = value;
+            });
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => ListProductsScreen(category: categories[value]),
+              )
+            );
+          },
+          children: [
+            ...categories.map((category) => NavigationDrawerDestination(
+                icon: Icon(category.icon),
+                label: Text(category.name))
+            ),
+          ]
         );
       },
-      children: [
-        ...categories.map((category) => NavigationDrawerDestination(
-          icon: Icon(category.icon),
-          label: Text(category.name))
-        ),
-      ]
     );
   }
 }
